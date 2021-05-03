9751798f9d36a80c956ca56b3ad61ca6862fdf3c
emmacary <emma.cary@tufts.edu>
2021-05-03 19:31:53 -0400

    Update routingPage.js

--- routingPage.js
+++ routingPage.js
@@ -8,7 +8,7 @@ var qs = require('querystring');
 var port = process.env.PORT || 3001;
 
 http.createServer(function (req, res) {
-    if (req.url == "plarent.herokuapp.com/" || req.url == "plarent.herokuapp.com/index.html")
+    if (req.url == "plarent.herokuapp.com" || req.url == "plarent.herokuapp.com/index.html")
     {
         file = 'index.html';
         fs.readFile(file, function(err, txt) {
